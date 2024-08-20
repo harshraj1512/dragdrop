4fb3ea606b6d3494006954a00a4145fd5f638b88
harshraj1512 <harshprogram63@gmail.com>
2024-08-20 22:11:36 +0530

    navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,55 +1,73 @@
 import React, { useState } from "react";
-import { DndContext } from "@dnd-kit/core"
+import { DndContext, closestCorners } from "@dnd-kit/core"
 import Draggable from "./components/Draggable"
 import Droppable from "./components/Droppable"
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
+
+
+// function App() {
+//   const containers = ['A', 'B', 'C'];
+//   const [parent, setParent] = useState({
+//     draggable1: null, 
+//     draggable2: null, 
+//   });
+//   // const draggableMarkup = (
+//   //   <Draggable id="draggable">Drag me</Draggable>
+//   // );
+
+//   function handleDragEnd(event) {
+//     const { over, active } = event;
+
+    
+//     setParent((prev) => ({
+//       ...prev,
+//       [active.id]: over ? over.id : null,
+//     }));
+//   }
+//   return (
+//     <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
+//       <div className="space-x-4">
+//       {parent.draggable1 === null && (
+//         <Draggable id="draggable1">Drag me</Draggable>
+//       )}
+//       {parent.draggable2 === null && (
+//         <Draggable id="draggable2">Here</Draggable>
+//       )}
+//       </div>
+//       {containers.map((id) => (
+//         // We updated the Droppable component so it would accept an `id`
+//         // prop and pass it to `useDroppable`
+//         <Droppable key={id} id={id}>
+//           {parent.draggable1 === id && (
+//             <Draggable id="draggable1">Drag me</Draggable>
+//           )}
+//           {parent.draggable2 === id && (
+//             <Draggable id="draggable2">Here</Draggable>
+//           )}
+//           {parent.draggable1 !== id &&
+//             parent.draggable2 !== id &&
+//             "Drop here"}
+//         </Droppable>
+//       ))}
+//     </DndContext>
+//   );
+ 
+// }
+
+// export default App
 
 
 function App() {
-  const containers = ['A', 'B', 'C'];
-  const [parent, setParent] = useState({
-    draggable1: null, 
-    draggable2: null, 
-  });
-  // const draggableMarkup = (
-  //   <Draggable id="draggable">Drag me</Draggable>
-  // );
-
-  function handleDragEnd(event) {
-    const { over, active } = event;
-
-    
-    setParent((prev) => ({
-      ...prev,
-      [active.id]: over ? over.id : null,
-    }));
-  }
+  
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      <div className="space-x-4">
-      {parent.draggable1 === null && (
-        <Draggable id="draggable1">Drag me</Draggable>
-      )}
-      {parent.draggable2 === null && (
-        <Draggable id="draggable2">Here</Draggable>
-      )}
-      </div>
-      {containers.map((id) => (
-        // We updated the Droppable component so it would accept an `id`
-        // prop and pass it to `useDroppable`
-        <Droppable key={id} id={id}>
-          {parent.draggable1 === id && (
-            <Draggable id="draggable1">Drag me</Draggable>
-          )}
-          {parent.draggable2 === id && (
-            <Draggable id="draggable2">Here</Draggable>
-          )}
-          {parent.draggable1 !== id &&
-            parent.draggable2 !== id &&
-            "Drop here"}
-        </Droppable>
-      ))}
-    </DndContext>
+    <>
+    <div className="flex flex-col min-h-screen min-w-full max-h-screen">
+    <Navbar/>
+    <Home/>
+    </div>
+    </>
   );
  
 }
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className="min-w-fit container ">
+      <div className="navbar bg-[#27272F] rounded-xl ">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Design Studio</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control relative">
+            <input
+              type="text"
+              placeholder="Search Project"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
+          <button className="btn btn-ghost border-solid border-2 border-sky-500">
+            <div className="indicator gap-2 items-center">
+              
+              <a>Save Layout</a>
+            </div>
+          </button>
+          <button className="btn btn-ghost border-solid border-2 border-sky-500">
+            <div className="indicator gap-2 items-center">
+              <a>Load layout</a>
+            </div>
+          </button>
+          <button className="btn btn-ghost border-solid border-2 border-sky-500">
+            <div className="indicator gap-2 items-center">
+              <a>publish</a>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Navbar
