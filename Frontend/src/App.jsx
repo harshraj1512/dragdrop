df3700a0a7bcd3450696127d05373ac8fdd54949
harshraj1512 <harshprogram63@gmail.com>
2024-08-20 19:01:32 +0530

    basic drag and drop

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { DndContext } from "@dnd-kit/core"
+import Draggable from "./components/Draggable"
+import Droppable from "./components/Droppable"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = (
+    <Draggable>Drag me</Draggable>
+  );
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <DndContext onDragEnd={handleDragEnd}>
+      {!isDropped ? draggableMarkup : null}
+      <Droppable>
+        {isDropped ? draggableMarkup : 'Drop here'}
+      </Droppable>
+    </DndContext>
+  );
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === 'droppable') {
+      setIsDropped(true);
+    }
+  }
 }
 
 export default App
---
+++ Frontend/src/components/Droppable.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useDroppable } from "@dnd-kit/core";
+
+function Droppable(props) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: "droppable",
+  });
+
+  const style = isOver
+    ? "border-2 border-blue-500 bg-white w-80 h-80"
+    : "border-2 border-blue-500 bg-white w-80 h-80";
+
+  return (
+    <div ref={setNodeRef} className={`${style} absolute bottom-0 right-0 m-4`}>
+      {props.children}
+    </div>
+  );
+}
+
+export default Droppable;
