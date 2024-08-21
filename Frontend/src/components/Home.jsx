feb02567b857b43e89b41ef879dfff515ea9e034
harshraj1512 <harshprogram63@gmail.com>
2024-08-21 17:54:33 +0530

    Drag and drop working

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useDroppable } from '@dnd-kit/core';
 
-function Home() {
-  const [dropped, setDropped] = useState([]); 
+function Home(props) {
+ 
   const { isOver, setNodeRef } = useDroppable({
     id: 'designer-drop-area',
   });
@@ -11,13 +11,13 @@ function Home() {
     ? "border-2 border-blue-500 bg-blue-100"
     : "border-2 border-blue-500";
 
-    
+
   return (
     <div ref={setNodeRef} className={`${style}  flex flex-col w-full border items-center justify-center m-2`}>
-      {dropped.length === 0 && <h3>Drop</h3>}
-      {dropped.map((item, index)=> (
+      {props.dropped.length === 0 && <h3>Drop</h3>}
+      {props.dropped.map((item, index)=> (
         <div key={index}>
-          {item.children}
+          {item.content}
         </div>
       ))}
     </div>
--- Frontend/src/homes/Mainhome.jsx
+++ Frontend/src/homes/Mainhome.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Home from '../components/Home'
 import { DndContext, closestCorners } from '@dnd-kit/core'
 
 function Mainhome() {
+  const [dropped, setDropped] = useState([]);
+
+  const handledrop = (event) => {
+    const {active, over} = event;
+
+    if(over && over.id === 'designer-drop-area'){
+      const dragged ={
+        id: active.id,
+        content: getId(active.id),
+      };
+
+      setDropped((prev) => [...prev, dragged]);
+    }
+  };
+
+  const getId = (id) =>{
+    switch (id){
+      case 'label1': return <label className='border-dashed border-2 w-40 text-center '>Label Box</label>
+      case 'input1': return <input type="text" placeholder="Text Field" className="p-1 rounded text-black w-full border-2 border-dashed" />
+      case 'checkbox1': return (<div className="flex items-center w-full border-dashed border-2 w-28"><input type="checkbox" className="mr-2 w-full " /><label>Checkbox</label></div>)
+      case 'button1': return <button className="bg-blue-500 text-white py-2 px-4 border-dashed border-2 rounded">Button</button>
+      default: return null
+    }
+
+  }
+
   return (
-    <DndContext collisionDetection={closestCorners}>
+    <DndContext collisionDetection={closestCorners} onDragEnd={handledrop}>
     <div className='flex flex-row flex-grow '>
         <Sidebar/>
-        <Home/>
+        <Home dropped={dropped}/>
     </div>
     </DndContext>
   )
